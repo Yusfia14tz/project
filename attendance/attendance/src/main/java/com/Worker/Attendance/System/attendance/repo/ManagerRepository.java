182383ef2ed1cdb1bf4839518b446d5dc05b7008
Yusfia14tz <yusfia14tz@gmail.com>
2024-07-10 12:24:00 -0500

    front

--- attendance/attendance/src/main/java/com/Worker/Attendance/System/attendance/repo/ManagerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Worker.Attendance.System.attendance.repo;
-
-import com.Worker.Attendance.System.attendance.model.Employee;
-import com.Worker.Attendance.System.attendance.model.Manager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerRepository extends JpaRepository<Manager, Integer> {
-}
